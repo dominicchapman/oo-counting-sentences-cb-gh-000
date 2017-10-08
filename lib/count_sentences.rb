7cc63ef730f6f21b44837a7914f281d355663751
Dominic Chapman <dominicchapman@me.com>
2017-10-08 16:34:36 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -9,7 +9,9 @@ class String
   end
 
   def question?
-
+    # returns true if the string that you are calling this method on ends in a question mark.
+    # returns false if the string that you are calling this method on does NOT end in question
+    self.end_with('?')
   end
 
   def exclamation?

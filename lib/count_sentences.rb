9d603225e247861cabd6fbe5b9d99938e3360271
Dominic Chapman <dominicchapman@me.com>
2017-10-08 16:34:08 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,9 @@ require 'pry'
 class String
 
   def sentence?
-    # self.end_with?('.')
+    # returns true if the string that you are calling this method on ends in a period.
+    # returns false if the string that you are calling this method on does NOT end in a period.
+    self.end_with?('.')
   end
 
   def question?
